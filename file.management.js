586a7ead4d343a3134538ec43def8edb8b1cc2e9
Nate Taylor <taylonr@gmail.com>
2019-12-24 12:55:11 -0600

    Ensure the file name is supplied.

--- file.management.js
+++ file.management.js
@@ -2,18 +2,34 @@ const { readdirSync, readFileSync, unlinkSync, writeFileSync } = require("fs");
 
 module.exports = {
   createFile: filename => {
+    if (!filename) {
+      throw new Error("Must supply file name");
+    }
+
     return writeFileSync(`./data/${filename}`, "", { flag: "wx" });
   },
   deleteFile: filename => {
+    if (!filename) {
+      throw new Error("Must supply file name");
+    }
+
     return unlinkSync(`./data/${filename}`);
   },
   getFile: filename => {
+    if (!filename) {
+      throw new Error("Must supply file name");
+    }
+
     return readFileSync(`./data/${filename}`);
   },
   getAllFiles: () => {
     return readdirSync("./data");
   },
   saveFile: (filename, contents) => {
+    if (!filename) {
+      throw new Error("Must supply file name");
+    }
+
     return writeFileSync(`./data/${filename}`, contents);
   }
 };
