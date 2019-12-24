c45e806087353823793cd804a361bc75a1132f6a
Nate Taylor <taylonr@gmail.com>
2019-12-24 12:48:07 -0600

    Initial test

---
+++ file.management.spy.spec.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const { spy } = require("sinon");
+const fs = require("fs");
+
+describe("File Management", () => {
+  it("true should be true", () => {
+    expect(true).to.be.true;
+  });
+});
