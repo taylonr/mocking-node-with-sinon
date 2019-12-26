2a5934fd13036d4fb4ace66a72f4aa36669d0d10
Nate Taylor <taylonr@gmail.com>
2019-12-26 17:25:34 -0600

    adding proxyquire

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { createSandbox, spy } = require("sinon");
 const fs = require("fs");
+const proxyquire = require("proxyquire");
 const fileManagement = require("./file.management");
 
 describe("File Management", () => {
