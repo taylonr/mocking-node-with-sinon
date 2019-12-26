571ff2290a04cb6371ea3b030adbadd6e87e945b
Nate Taylor <taylonr@gmail.com>
2019-12-26 13:57:17 -0600

    First stub test

--- file.management.stub.spec.js
+++ file.management.stub.spec.js
@@ -1,6 +1,21 @@
 const { expect } = require("chai");
-const { createSandbox, spy } = require("sinon");
+const { createSandbox } = require("sinon");
 const fs = require("fs");
-const fileManagement = require("./file.management");
 
-describe("File Management", () => {});
+describe.only("File Management Stub", () => {
+  const sandbox = createSandbox();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("Should create a file", () => {
+    const writeStub = sandbox.stub(fs, "writeFileSync");
+
+    const fileManagement = require("./file.management");
+
+    fileManagement.createFile("test.txt");
+
+    expect(writeStub.callCount).to.equal(1);
+  });
+});
