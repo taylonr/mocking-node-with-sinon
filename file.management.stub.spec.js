e6b56fc1872128e923a3820e4774d3e5c69d739e
Nate Taylor <taylonr@gmail.com>
2019-12-26 10:33:46 -0600

    Adding the stub spec file.

---
+++ file.management.stub.spec.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const { createSandbox, spy } = require("sinon");
+const fs = require("fs");
+const fileManagement = require("./file.management");
+
+describe("File Management", () => {
+  it("true should be true", () => {
+    expect(true).to.be.true;
+  });
+});
