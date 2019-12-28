f124d7d8b859db487bbe9332078d85b7330a16b4
Nate Taylor <taylonr@gmail.com>
2019-12-28 10:21:12 -0600

    Removing stub file from master

--- file.management.stub.spec.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require("chai");
-const { createSandbox, spy } = require("sinon");
-const fs = require("fs");
-
-describe("File Management", () => {
-  it("true should be true", () => {
-    expect(true).to.be.true;
-  });
-});
