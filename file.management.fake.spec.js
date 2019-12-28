1b8319bf651c8bd0af90c0e498c2899f554bc7f0
Nate Taylor <taylonr@gmail.com>
2019-12-27 22:04:23 -0600

    Completing the mock tests

--- file.management.fake.spec.js
+++ file.management.fake.spec.js
@@ -3,7 +3,7 @@ const sinon = require("sinon");
 const fs = require("fs");
 const proxyquire = require("proxyquire");
 
-describe("File Management Fake", () => {
+describe.skip("File Management Fake", () => {
   afterEach(() => {
     sinon.restore();
   });
--- file.management.mock.spec.js
+++ file.management.mock.spec.js
@@ -8,7 +8,51 @@ describe("File Management Mocks", () => {
     sinon.restore();
   });
 
-  it("True should be true", () => {
-    expect(true).to.be.true;
+  it("Should call writeFileSync when creating a test", () => {
+    const writeMock = sinon.mock(fs);
+    writeMock.expects("writeFileSync").once();
+
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    fileManagement.createFile("test.txt");
+
+    writeMock.verify();
+  });
+
+  it("createFileSafe should create a new file with a number appended", () => {
+    const writeMock = sinon.mock(fs);
+    writeMock
+      .expects("writeFileSync")
+      .withArgs("./data/test.txt")
+      .throws();
+
+    writeMock
+      .expects("writeFileSync")
+      .withArgs("./data/test1.txt")
+      .returns(1);
+
+    writeMock
+      .expects("readdirSync")
+      .returns(["test.txt"])
+      .once();
+
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    fileManagement.createFileSafe("test.txt");
+
+    writeMock.verify();
+  });
+
+  it("createFile should never call writeFileSync when the file is empty", () => {
+    const writeMock = sinon.mock(fs);
+    writeMock.expects("writeFileSync").never();
+
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    try {
+      fileManagement.createFile();
+    } catch (err) {}
+
+    writeMock.verify();
   });
 });
